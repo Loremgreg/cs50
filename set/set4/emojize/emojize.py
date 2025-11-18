ff3402c980148a22a138772a1d99b03625dc2d35
Loremgreg <gregorybastin.de@gmail.com>
2025-11-18 15:59:52 +0100

    a

---
+++ set/set4/emojize/emojize.py
@@ -0,0 +1,14 @@
+import emoji
+
+
+def main():
+    user_input = input()
+
+    output = emojized(user_input)
+    print(output)
+
+def emojized(user_input):
+    emo = emoji.emojize(user_input, language='alias')
+    return emo
+
+main()
